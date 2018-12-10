28fde863f71e9c2b8a2144d2cbc32cd735dbf43d
iraK21 <irakawthalkar21@gmail.com>
2018-12-10 22:31:58 +0530

    Initial commit

--- src/main/java/com/example/ira/App.java
+++ src/main/java/com/example/ira/App.java
@@ -1,9 +1,7 @@
 package com.example.ira;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
